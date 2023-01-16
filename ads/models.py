b3fcff72d78a900b5ab3402747c7470e06cb8b59
xytki <skomorosi@gmail.com>
2023-01-16 11:44:41 +0100

    my_2_commit

--- ads/models.py
+++ ads/models.py
@@ -10,7 +10,7 @@ from django.db import models
 class Ad(models.Model):
     name = models.CharField(max_length=150)
     author = models.CharField(max_length=50)
-    price = models.PositiveIntegerField()
+    price = models.IntegerField()
     description = models.TextField()
     address = models.CharField(max_length=300)
     is_published = models.BooleanField()
